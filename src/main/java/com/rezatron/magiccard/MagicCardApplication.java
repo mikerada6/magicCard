73ae5fce5762fa69412948389f59d471ee36fa0d
Michael Radaszkiewicz <michael_radaszkiewicz@comcast.com>
2022-11-15 08:24:22 -0500

    initial commit

---
+++ src/main/java/com/rezatron/magiccard/MagicCardApplication.java
@@ -0,0 +1,16 @@
+package com.rezatron.magiccard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public
+class MagicCardApplication {
+
+  public static
+  void main(String[] args) {
+    SpringApplication.run( MagicCardApplication.class,
+                           args );
+  }
+
+}
---
+++ src/test/java/com/rezatron/magiccard/MagicCardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rezatron.magiccard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class MagicCardApplicationTests {
+
+  @Test
+  void contextLoads() {
+    assertTrue(true);
+  }
+
+}
